542d4e6af4a44d9593264754188f871f0444e9d5
surovishimu <surovi.shimu95@gmail.com>
2023-09-01 05:14:24 +0600

    all card added dynamically

---
+++ index.js
@@ -0,0 +1,104 @@
+const loadAllCategory = async () => {
+    try {
+        const response = await fetch('https://openapi.programming-hero.com/api/videos/categories');
+        const data = await response.json();
+        const load = data.data;
+        return load;
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+const setCategory = async () => {
+    const data = await loadAllCategory();
+    const categoryUl = document.getElementById('all-category');
+    for (const category of data) {
+        // console.log(category);
+        const li = document.createElement('li');
+        li.innerHTML = `
+        <li onclick="loadUrl('${category.category_id}')" class="btn mt-5 normal-case ">${category.category}</li>
+        `
+        categoryUl.appendChild(li);
+
+
+    }
+}
+setCategory()
+
+const loadUrl = async (idURL) => {
+
+    const spinner = document.getElementById('spinner');
+    const url = ` https://openapi.programming-hero.com/api/videos/category/${idURL}`;
+    const response = await fetch(url);
+    const loadImg = await response.json();
+    displayNews(loadImg.data);
+    //
+    console.log(loadImg.data);
+}
+
+const secondsToTime = seconds => {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+
+    if (!seconds) {
+        return `${""}`;
+    }
+    else {
+        return `${hours} hr ${minutes} min ago`;
+    }
+
+};
+
+const displayNews = (displayCard) => {
+    const cardDetails = document.getElementById('card-container');
+    const notFound = document.getElementById('not-found')
+    cardDetails.textContent = "";
+    notFound.textContent = "";
+
+    if (displayCard.length === 0) {
+        notFound.innerHTML = `
+        <img class="mx-auto mt-10" src="./image/Icon.png" alt="">
+        <h2 class="mt-5 p-5 text-2xl font-semibold text-black text-center">Oops!! Sorry, There is no <br> content here.</h2>`
+        return;
+    }
+
+
+    displayCard.forEach(newsCard => {
+        console.log(newsCard
+        );
+        const newsCardDiv = document.createElement('div');
+        // newsCardDiv.classList.add('');
+        newsCardDiv.innerHTML = `
+        <div class="card card-compact  ">
+                <figure class="relative"><img class="w-72 h-48 rounded-lg mb-3" src="${newsCard.thumbnail ? newsCard.thumbnail : 'not found'}" alt="Shoes" />
+                <p class="absolute bottom-4 right-2 bg-gray-600 text-white rounded-sm text-xs">
+                ${secondsToTime(newsCard.others.posted_date ? newsCard.others.posted_date : "")}
+                </p>
+                
+                </figure>
+                <div class=" flex lg:justify-start justify-center px-2 gap-4 ">
+                 <div class=""><img class="w-12 h-12 rounded-full" src="${newsCard.authors[0].profile_picture ? newsCard.authors[0].profile_picture : 'not found'}" alt="Shoes" /></div>
+                 <div>
+                 <div>
+                 <h2 class="text-xl font-semibold">${newsCard.title}</h2>
+                 <div class='flex gap-4'><p class="text-slate-400">${newsCard.authors[0].profile_name}</p>
+                 <i>${newsCard.authors[0].verified ? '<span class="verified-symbol"><i class="fa-solid fa-certificate text-sky-400"></i></span>' : ''} </i>
+                 </div>
+                 <p class="text-slate-400 text-xs">${newsCard.others.views} views</p>
+                 </div> </div>  
+
+
+
+
+                </div>
+                
+             
+                       
+            
+            </div>
+        `
+        cardDetails.appendChild(newsCardDiv)
+    });
+}
+loadAllCategory()
